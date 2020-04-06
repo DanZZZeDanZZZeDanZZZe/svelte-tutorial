658e5e7716a2107eec2974418d3048d045cccc9a
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-04-06 16:06:54 +0300

    add project

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import "../node_modules/materialize-css/dist/css/materialize.min.css";
+  import "../node_modules/materialize-css/dist/js/materialize.min.js";
+
+  import { Router, Link, Route } from "svelte-routing";
+
+  import Navabr from "./layout/Navbar.svelte";
+
+  import About from "./pages/About.svelte";
+  import Home from "./pages/Home.svelte";
+</script>
+
+<Router>
+  <Navabr />
+  <div class="container">
+    <Route path="/" component={Home} />
+    <Route path="/about" component={About} />
+  </div>
+</Router>
